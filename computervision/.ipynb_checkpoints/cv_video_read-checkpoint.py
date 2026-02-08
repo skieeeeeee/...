7cac37bc43e291f748bbe1902caad1ecc9ed9dc8
Akash Bhandari <akashbhandari8255@gmail.com>
2026-02-08 10:28:57 +0530

    regression

--- computervision/.ipynb_checkpoints/cv_video_read-checkpoint.py
+++ computervision/.ipynb_checkpoints/cv_video_read-checkpoint.py
@@ -1,6 +1,7 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 
 while True :
     ret ,frame = cap.read()
@@ -8,9 +9,13 @@ while True :
     if ret == False:
         continue
 
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(frame, 1.3,5)
+    for(x,y,w,h) in faces :
+        cv2.rectangle(frame,(x,y) , (x+w,y+h),[255,0,0],2)
+
+    #gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     cv2.imshow('captured image', frame)
-    cv2.imshow('gray image', gray)
+    #cv2.imshow('gray image', gray)
     key = cv2.waitKey(0)
     if key == ord('q'):
         break
